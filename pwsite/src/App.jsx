639bea8b118490affb2c6a9b6e6a5cb31a3d2d87
amamoon <akeefmamoon@gmail.com>
2021-10-20 21:25:00 +1100

    added menus

--- pwsite/src/App.jsx
+++ pwsite/src/App.jsx
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Contact from './components/contact/Contact';
 import Portfolio from './components/portfolio/Portfolio';
 import Works from './components/works/Works';
+import Menu from './components/menu/Menu'
 import "./app.scss"
 
 function App() {
+  const[menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="App">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className='sections'>
         <Intro/>
         <Contact/>
--- pwsite/src/components/works/Works.jsx
+++ pwsite/src/components/works/Works.jsx
@@ -3,7 +3,7 @@ import './works.scss'
 
 export default function works() {
     return (
-        <div className='works'>
+        <div className='works' id="works">
             work
         </div>
     )
